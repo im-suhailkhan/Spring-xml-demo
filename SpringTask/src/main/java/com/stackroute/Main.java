9bd9bf19f3b30c3adb5af55dbeae9107c38eae61
suhail.khan <su.khan@cgi.com>
2019-10-12 01:52:26 +0530

    refactored class folder in domain

--- SpringTask/src/main/java/com/stackroute/Main.java
+++ SpringTask/src/main/java/com/stackroute/Main.java
@@ -1,12 +1,9 @@
 package com.stackroute;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
+import com.stackroute.domain.Movie;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-import org.w3c.dom.ls.LSOutput;
 
 public class Main {
 
